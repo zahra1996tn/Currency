92f4a3d29c04621e1443305ca088c45b09873fdb
zahra <z.tanhaei1996@gmail.com>
2022-05-08 00:39:54 +0430

    first commit

---
+++ src/App.vue
@@ -0,0 +1,206 @@
+<template>
+  <div class="header">
+    <div class="nav">
+    </div>
+    <h1 class="title">Currency Converter</h1>
+    <h3 class="sub-title">check live foreign currency exchange reates !</h3>
+  </div>
+  <div id="app1">
+    <div>
+      <h1 class="title2">exchange rate<i class="fa fa-money"></i></h1>
+    </div>
+    <div class="container">
+      <div class="container-one">
+        <input
+          type="number"
+          name="input-one"
+          id="input-one"
+          v-model="amountOne"
+          @input="fetchData()"
+        />
+        <select
+          name="first-currency"
+          id="first-currency"
+          v-model="currency_one"
+          @change="fetchData()"
+        >
+          <option value="AED">AED</option>
+          <option value="ARS">ARS</option>
+          <option value="AUD">AUD</option>
+          <option value="BGN">BGN</option>
+          <option value="BRL">BRL</option>
+          <option value="BSD">BSD</option>
+          <option value="CAD">CAD</option>
+          <option value="CHF">CHF</option>
+          <option value="CLP">CLP</option>
+          <option value="CNY">CNY</option>
+          <option value="COP">COP</option>
+          <option value="CZK">CZK</option>
+          <option value="DKK">DKK</option>
+          <option value="DOP">DOP</option>
+          <option value="EGP">EGP</option>
+          <option value="EUR">EUR</option>
+          <option value="FJD">FJD</option>
+          <option value="GBP">GBP</option>
+          <option value="GTQ">GTQ</option>
+          <option value="HRK">HRK</option>
+          <option value="HUF">HUF</option>
+          <option value="IDR">IDR</option>
+          <option value="ILS">ILS</option>
+          <option value="INR">INR</option>
+          <option value="INR">IRR</option>
+          <option value="ISK">ISK</option>
+          <option value="JPY">JPY</option>
+          <option value="KRW">KRW</option>
+          <option value="KZT">KZT</option>
+          <option value="MXN">MXN</option>
+          <option value="MYR">MYR</option>
+          <option value="NOK">NOK</option>
+          <option value="NZD">NZD</option>
+          <option value="PAB">PAB</option>
+          <option value="PEN">PEN</option>
+          <option value="PHP">PHP</option>
+          <option value="PKR">PKR</option>
+          <option value="PLN">PLN</option>
+          <option value="PYG">PYG</option>
+          <option value="RON">RON</option>
+          <option value="RUB">RUB</option>
+          <option value="SAR">SAR</option>
+          <option value="SEK">SEK</option>
+          <option value="SGD">SGD</option>
+          <option value="THB">THB</option>
+          <option value="TRY">TRY</option>
+          <option value="TWD">TWD</option>
+          <option value="UAH">UAH</option>
+          <option value="USD">USD</option>
+          <option value="UYU">UYU</option>
+          <option value="VND">VND</option>
+          <option value="ZAR">ZAR</option>
+        </select>
+        <select
+          name="second-currency"
+          id="second-currency"
+          v-model="currency_two"
+          @change="fetchData()"
+        >
+          <option value="AED">AED</option>
+          <option value="ARS">ARS</option>
+          <option value="AUD">AUD</option>
+          <option value="BGN">BGN</option>
+          <option value="BRL">BRL</option>
+          <option value="BSD">BSD</option>
+          <option value="CAD">CAD</option>
+          <option value="CHF">CHF</option>
+          <option value="CLP">CLP</option>
+          <option value="CNY">CNY</option>
+          <option value="COP">COP</option>
+          <option value="CZK">CZK</option>
+          <option value="DKK">DKK</option>
+          <option value="DOP">DOP</option>
+          <option value="EGP">EGP</option>
+          <option value="EUR">EUR</option>
+          <option value="FJD">FJD</option>
+          <option value="GBP">GBP</option>
+          <option value="GTQ">GTQ</option>
+          <option value="HRK">HRK</option>
+          <option value="HUF">HUF</option>
+          <option value="IDR">IDR</option>
+          <option value="ILS">ILS</option>
+          <option value="INR">INR</option>
+          <option value="INR">IRR</option>
+          <option value="ISK">ISK</option>
+          <option value="JPY">JPY</option>
+          <option value="KRW">KRW</option>
+          <option value="KZT">KZT</option>
+          <option value="MXN">MXN</option>
+          <option value="MYR">MYR</option>
+          <option value="NOK">NOK</option>
+          <option value="NZD">NZD</option>
+          <option value="PAB">PAB</option>
+          <option value="PEN">PEN</option>
+          <option value="PHP">PHP</option>
+          <option value="PKR">PKR</option>
+          <option value="PLN">PLN</option>
+          <option value="PYG">PYG</option>
+          <option value="RON">RON</option>
+          <option value="RUB">RUB</option>
+          <option value="SAR">SAR</option>
+          <option value="SEK">SEK</option>
+          <option value="SGD">SGD</option>
+          <option value="THB">THB</option>
+          <option value="TRY">TRY</option>
+          <option value="TWD">TWD</option>
+          <option value="UAH">UAH</option>
+          <option value="USD">USD</option>
+          <option value="UYU">UYU</option>
+          <option value="VND">VND</option>
+          <option value="ZAR">ZAR</option>
+        </select>
+      </div>
+      <div class="container-two">
+        <h3 id="baseValue">
+          1 {{ currency_one }} = {{ rate }} {{ currency_two }}
+        </h3>
+      </div>
+      <div class="container-three">
+        <input
+          type="number"
+          id="amount-two"
+          placeholder="0"
+          disabled
+          v-model="amountTwo"
+        />
+      </div>
+      <div class="container-four">
+        <h4 id="lastlyupdated">
+          lastly updated: <strong> {{ data.time_last_update_utc }} </strong>
+        </h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+  },
+  data() {
+    return {
+      data: [],
+      currency_one: "USD",
+      currency_two: "EUR",
+      rate: "",
+      amountOne: 1,
+      amountTwo: 0,
+    };
+  },
+  methods: {
+    fetchData() {
+      fetch(
+        `https://v6.exchangerate-api.com/v6/1c9ccafdd776ce113ba7d0d8/latest/${this.currency_one}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          this.data = data;
+          this.rate = data.conversion_rates[this.currency_two];
+          this.amountTwo = this.amountOne * this.rate.toFixed(2);
+        });
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+@import "./assets/style.css";
+</style>
